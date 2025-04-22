83b8dbdae72972f67586a117a4f3946f39e3c54a
mariam6697 <mariam.maldonado.marin@gmail.com>
2025-04-22 19:42:02 -0400

    feat: first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  css: ['~/assets/css/main.css'],
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})
