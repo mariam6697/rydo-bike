60fb17b409d1dda8cbdb40a36186ef9413d6d3ff
mariam6697 <mariam.maldonado.marin@gmail.com>
2025-04-23 13:18:16 -0400

    refactor: navbar content

--- components/nav-bar.vue
+++ components/nav-bar.vue
@@ -3,16 +3,30 @@
         <div class="container mx-auto flex items-center justify-between">
             <NuxtLink to="/"><img src="/assets/images/rydo.png" alt="Rydo logo" width="150"></NuxtLink>
             <div class="space-x-4">
-                <NuxtLink to="/signup" class="text-gray-800 hover:text-gray-600">Signup</NuxtLink>
-                <NuxtLink to="/about" class="text-gray-800 hover:text-gray-600">About</NuxtLink>
-                <NuxtLink to="/contact" class="text-gray-800 hover:text-gray-600">Contact</NuxtLink>
+                <NuxtLink v-for="element in navBarElements" :key="element.id" :to="element.path" class="text-gray-800">
+                    {{ element.label }}
+                </NuxtLink>
             </div>
         </div>
     </nav>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'NavBar'
-}
+<script setup lang="ts">
+const navBarElements = [
+    {
+        id: 0,
+        path: '/signup',
+        label: 'Signup'
+    },
+    {
+        id: 1,
+        path: '/about',
+        label: 'About'
+    },
+    {
+        id: 2,
+        path: '/contact',
+        label: 'Contact'
+    }
+];
 </script>
