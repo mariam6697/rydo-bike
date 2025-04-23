b528fa9c80b881c7798a72293b50b253d53508db
mariam6697 <mariam.maldonado.marin@gmail.com>
2025-04-23 13:31:17 -0400

    feat: add footer component

---
+++ components/main-footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <footer class="bg-gray-900 text-neutral-50 p-4 h-48 flex flex-col items-center justify-center">
+        <img src="/assets/images/rydo_alt.png" alt="Rydo logo" width="200">
+        <p> &copy; {{ currentYear }} Rydo Bike. All rights reserved.    </p>
+    </footer>
+</template>
+
+<script setup lang="ts">
+const currentYear = ref(new Date().getFullYear());
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,11 @@
     <UApp>
         <NavBar />
         <NuxtPage />
+        <MainFooter />
     </UApp>
 </template>
 
 <script setup>
 import NavBar from '@/components/nav-bar.vue';
+import MainFooter from '~/components/main-footer.vue';
 </script>
