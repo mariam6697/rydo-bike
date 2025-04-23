0d978ce934581736dcc62ad1262ddc6268685194
mariam6697 <mariam.maldonado.marin@gmail.com>
2025-04-23 15:11:55 -0400

    feat: add head title + favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Rydo - Your Ride, Anywhere',
+  },
+  },
   compatibilityDate: '2024-11-01',
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
